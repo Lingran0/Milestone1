4cee75fafbd490c1315cea5674cb56d54c1095df
Lingran0 <150463096+Lingran0@users.noreply.github.com>
2024-10-16 12:11:49 -0400

    Add files via upload

---
+++ Codes/Main.java
@@ -0,0 +1,30 @@
+import textProcessor.textProcessor;
+import statisticCalculator.statisticCalculator;
+
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    
+        textProcessor textProcessor;
+        statisticCalculator statisticCalculator = new statisticCalculator();
+        
+        
+    }
+    
+    private static void removeStopWords(Scanner scanner, textProcessor textProcessor) throws FileNotFoundException {
+        
+    }
+
+    private static void calculateBasicStats(Scanner scanner, textProcessor textProcessor, statisticCalculator statisticCalculator) throws FileNotFoundException {
+       
+    }
+
+    private static void rankWordByFrequency(Scanner scanner, textProcessor textProcessor, statisticCalculator statisticCalculator) throws FileNotFoundException {
+       
+    }
+}
+
